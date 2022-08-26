ddb9a155471380d4952fcc3ebc56982aa833d962
dineesha29061994 <109737370+dineesha29061994@users.noreply.github.com>
2022-08-26 14:34:21 +0530

    Create tomcatDeploy.groovy

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,10 @@
+def call(ip,user,credid){
+  sshagent([credid]){
+        // copy
+        sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat3/webapps"
+        // stop
+       sh "ssh -o StrictHostKeyChecking=no ${user}@${ip} :/opt/tomcat3/bin/shutdown.sh"
+       // start
+       sh "ssh ${user}@${ip}: /opt/tomcat3/bin/startup.sh"
+  }
+}
